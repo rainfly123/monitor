49c6074ef5c356ba9ed587648482dd7d3282d909
xiechangcai <xiechangcai@southtv.cn>
2018-06-27 09:00:03 +0800

    d

--- watch.sh
+++ watch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+./watch.py
+sleep 3
 while [ 1 ]
 do
 for line in $(cat .chan)
 do
-ffplay -x 960 -y 540 -fs $line &
-sleep 19
+ffplay -loglevel error -x 960 -y 540 -fs $line &
+sleep 15
 kill -SIGINT $!
 done
 done
